7cf5625649fbd11de1c64c764b35b231a59e1ea0
ericbrown13 <ericbrown1.2018@u.northwestern.edu>
2016-09-10 18:11:18 -0700

    Add files via upload

---
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,68 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+c = size(theta,1);
+
+h = sigmoid(X * theta); % 118 x 1
+
+p1 = -y' * log(h);  % 1 x 1
+p2 = (1 - y)' * log(1 - h); % 1 x 1
+
+reg = lambda/(2 * m) * sum(theta(2:c,:).^2);
+
+J = 1/m * (p1 - p2) + reg;
+
+grad(1) = 1/m * (X(:,1)' * (h - y) ); % gradient for theta(0)
+
+gp1 = (1/m * (X(:,2:c)' * (h - y)));
+gp2 = (theta(2:c,:) * lambda/m);
+
+grad(2:c)= gp1 + gp2 ; % gradient for theta>1
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
